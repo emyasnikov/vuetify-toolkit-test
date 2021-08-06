33fa49ead34b4cb1b51616288511048ff2dff23f
Evgenij Myasnikov <emyasnikov@mkw.eu>
2021-08-06 10:38:20 +0200

    Changed webpack config. Added basic app component.

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app id="app" />
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass">
+@import '~@mdi/font/css/materialdesignicons.css'
+@import '~vuetify/dist/vuetify.min.css'
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import App from './App.vue'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
